9b078fc0acf6ef6c563881c9dc6f239c4012a2a9
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2020-08-10 11:54:42 +0200

    :sparkle: Used rustfmt --edition 2018 on source code

--- src/main.rs
+++ src/main.rs
@@ -11,25 +11,23 @@ struct Level {
     id: u8,
     scale: f64,
     directions: Vec<u8>,
-    types: Vec<SegmentType>
+    types: Vec<SegmentType>,
 }
 
-fn create_initial_level() -> Level
-{
-    Level{id: 0_u8,
-          scale: 7.0_f64.sqrt() / 2.0_f64,
-          directions: vec![0_u8],
-          types: vec![SegmentType::Type1]
-        }
+fn create_initial_level() -> Level {
+    Level {
+        id: 0_u8,
+        scale: 7.0_f64.sqrt() / 2.0_f64,
+        directions: vec![0_u8],
+        types: vec![SegmentType::Type1],
+    }
 }
 
-fn create_the_vector_of_levels() -> Vec<Level>
-{
+fn create_the_vector_of_levels() -> Vec<Level> {
     vec![create_initial_level()]
 }
 
-fn create_new_level(source_level: &Level) -> Level
-{
+fn create_new_level(source_level: &Level) -> Level {
     // t1 = 'abbaaab'
     let d1: Vec<u8> = vec![0_u8, 5_u8, 3_u8, 4_u8, 0_u8, 0_u8, 1_u8];
 
@@ -44,41 +42,47 @@ fn create_new_level(source_level: &Level) -> Level
             match source_level.types[i] {
                 SegmentType::Type1 => {
                     match j {
-                        0 | 3 | 4 | 5 => {types.push(SegmentType::Type1);}
-                        1 | 2 | 6 => {types.push(SegmentType::Type2);}
-                        _ => {},
+                        0 | 3 | 4 | 5 => {
+                            types.push(SegmentType::Type1);
+                        }
+                        1 | 2 | 6 => {
+                            types.push(SegmentType::Type2);
+                        }
+                        _ => {}
                     }
-                    directions.push((source_level.directions[i] + d1[j])%6_u8);
+                    directions.push((source_level.directions[i] + d1[j]) % 6_u8);
                 }
                 SegmentType::Type2 => {
                     match j {
-                        0 | 4 | 5 => {types.push(SegmentType::Type1);}
-                        1 | 2 | 3 | 6 => {types.push(SegmentType::Type2);}
-                        _ => {},
+                        0 | 4 | 5 => {
+                            types.push(SegmentType::Type1);
+                        }
+                        1 | 2 | 3 | 6 => {
+                            types.push(SegmentType::Type2);
+                        }
+                        _ => {}
                     }
-                    directions.push((source_level.directions[i] + d2[j])%6_u8);
+                    directions.push((source_level.directions[i] + d2[j]) % 6_u8);
                 }
             }
         }
     }
-    Level{id: source_level.id + 1_u8,
-          scale: source_level.scale / 7.0_f64.sqrt(),
-          directions: directions,
-          types: types
-      }
+    Level {
+        id: source_level.id + 1_u8,
+        scale: source_level.scale / 7.0_f64.sqrt(),
+        directions: directions,
+        types: types,
+    }
 }
 
-fn append_new_level(levels: &mut Vec<Level>)
-{
-    if let Some(last_level) = levels.last()
-    {
+fn append_new_level(levels: &mut Vec<Level>) {
+    if let Some(last_level) = levels.last() {
         let new_level = create_new_level(last_level);
         levels.push(new_level);
     }
 }
 
-fn create_gosper_fractal(max_level: u8) -> Vec<Level>
-{
+fn create_gosper_fractal(max_level: u8) -> Vec<Level> {
     let mut levels = create_the_vector_of_levels();
     for _i in 1..=max_level {
         append_new_level(&mut levels);
@@ -86,8 +90,7 @@ fn create_gosper_fractal(max_level: u8) -> Vec<Level>
     levels
 }
 
-fn cosinus(key: u8) -> f64
-{
+fn cosinus(key: u8) -> f64 {
     // Returns the cosinus value of an angle described
     // by a key that is a multiple of pi/3.
     // Here, k1 = cos(pi/3)
@@ -103,8 +106,7 @@ fn cosinus(key: u8) -> f64
     }
 }
 
-fn sinus(key: u8) -> f64
-{
+fn sinus(key: u8) -> f64 {
     // Returns the sinus value of an angle described
     // by a key that is a multiple of pi/3.
     // Here, k2 = sin(pi/3)
@@ -120,8 +122,7 @@ fn sinus(key: u8) -> f64
     }
 }
 
-fn generate_level(level: &Level) -> Vec<(f32, f32)>
-{
+fn generate_level(level: &Level) -> Vec<(f32, f32)> {
     // Convert the formal description of a level to a x, y curve.
     // Result is casted to a vector of tuples of float32 so that
     // it can directly be used by the plotting library
@@ -129,9 +130,8 @@ fn generate_level(level: &Level) -> Vec<(f32, f32)>
     let n = level.directions.len();
     let mut x: Vec<f64> = vec![0.0_f64; n + 1];
     let mut y: Vec<f64> = vec![0.0_f64; n + 1];
-    let mut res: Vec<(f32, f32)>= vec![(0.0_f32, 0.0_f32); n + 1];
-    for i in 0..n
-    {
+    let mut res: Vec<(f32, f32)> = vec![(0.0_f32, 0.0_f32); n + 1];
+    for i in 0..n {
         x[i + 1] = x[i] + scale * cosinus(level.directions[i]);
         y[i + 1] = y[i] + scale * sinus(level.directions[i]);
     }
@@ -140,35 +140,35 @@ fn generate_level(level: &Level) -> Vec<(f32, f32)>
     let alpha = index * ((3.0_f64.sqrt() / 5.0).atan());
     let cos_alpha = alpha.cos();
     let sin_alpha = alpha.sin();
-    for i in 0..(n+1)
-    {
-        res[i]= ((cos_alpha * x[i] - sin_alpha * y[i]) as f32,
-                 (sin_alpha * x[i] + cos_alpha * y[i]) as f32);
+    for i in 0..(n + 1) {
+        res[i] = (
+            (cos_alpha * x[i] - sin_alpha * y[i]) as f32,
+            (sin_alpha * x[i] + cos_alpha * y[i]) as f32,
+        );
     }
     res
 }
 
-fn plot_level(level: &Level) -> Result<(), Box<dyn std::error::Error>>
-{
+fn plot_level(level: &Level) -> Result<(), Box<dyn std::error::Error>> {
     let filename = format!("{}.png", level.id.to_string());
-    let root = BitMapBackend::new(&filename, (640*2, 640*2)).into_drawing_area();
+    let root = BitMapBackend::new(&filename, (640 * 2, 640 * 2)).into_drawing_area();
     root.fill(&WHITE)?;
     let mut chart = ChartBuilder::on(&root)
         // .caption("y=x^2", ("sans-serif", 50).into_font())
         .margin(1)
         // .x_label_area_size(30)
         // .y_label_area_size(30)
-        .build_ranged(-0.25f32..(7.0f32.sqrt() / 2.0_f32 + 0.25f32), -1.25f32..0.75f32)?;
-        //.build_ranged(-0.5f32..1.5f32, -1.5f32..0.5f32)?;
+        .build_ranged(
+            -0.25f32..(7.0f32.sqrt() / 2.0_f32 + 0.25f32),
+            -1.25f32..0.75f32,
+        )?;
+    //.build_ranged(-0.5f32..1.5f32, -1.5f32..0.5f32)?;
 
     // chart.configure_mesh().draw()?;
 
-    chart
-        .draw_series(LineSeries::new(generate_level(level),
-            &BLACK,
-        ))?;
-        // .label("y = x^2");
-        // .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+    chart.draw_series(LineSeries::new(generate_level(level), &BLACK))?;
+    // .label("y = x^2");
+    // .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
 
     chart
         .configure_series_labels()
@@ -178,12 +178,10 @@ fn plot_level(level: &Level) -> Result<(), Box<dyn std::error::Error>>
     Ok(())
 }
 
-fn main()
-{
+fn main() {
     let maximum_number_of_levels = 7_u8;
     let levels = create_gosper_fractal(maximum_number_of_levels);
-    for level in levels
-    {
+    for level in levels {
         let _result = plot_level(&level);
     }
 }
